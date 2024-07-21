a3239073d8efb5ee2d7e495f98c06fe48b1fdef2
Junio Souza <ejunioms@gmail.com>
2024-07-21 16:23:54 -0300

    feat: Adding locale pt-br provide

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(ptBr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +16,8 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),
+    {
+      provide: LOCALE_ID, useValue: 'pt',
+    },
   ]
 };
