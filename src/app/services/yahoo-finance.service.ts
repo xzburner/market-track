db0d66d0693f8c21f4a14bf1a82ba9c5aba4f348
Junio Souza <ejunioms@gmail.com>
2024-07-23 17:52:31 -0300

    feat: Correcting api url to avoid cors error

--- src/app/services/yahoo-finance.service.ts
+++ src/app/services/yahoo-finance.service.ts
@@ -9,7 +9,7 @@ export class YahooFinanceService {
 
   constructor(private http: HttpClient) { }
 
-  private apiUrl = 'http://localhost:3000/historical-data';
+  private apiUrl = 'http://18.230.187.246:3000/historical-data';
 
   getHistoricalData(symbol: string, startDate: string, endDate: string): Observable<any> {
     console.log(symbol);
